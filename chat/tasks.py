33602539543bc6aecf94b0602f2501f8fb854568
Abhay Singh <abhay97ps@gmail.com>
2025-03-07 01:21:04 -0500

    migrate: ingest sync to async

--- chat/tasks.py
+++ chat/tasks.py
@@ -1,3 +1,4 @@
+from .ingest import ingest_individual, ingest_group_sync
 from config.celery import app
 import logging
 from celery import shared_task
@@ -15,3 +16,13 @@ logger = logging.getLogger(__name__)
 def sample_task():
     print("Celery beat: Running sample task!")
     logger.info("Celery beat: Running sample task!")
+
+
+@shared_task
+def ingest_individual_task(participant_id, data):
+    ingest_individual(participant_id, data)
+
+
+@shared_task
+def ingest_group_task(group_id, data):
+    ingest_group_sync(group_id, data)
