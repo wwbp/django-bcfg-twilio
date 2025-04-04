cf38396f5324fd419be4914547f92d2cbe89ce5c
Abhay Singh <abhay97ps@gmail.com>
2025-04-04 12:09:19 -0400

    pull moderation category value json from config

--- config/settings.py
+++ config/settings.py
@@ -178,3 +178,21 @@ OPENAI_API_KEY = os.environ.get("OPENAI_API_KEY", "")
 BCFG_DOMAIN = os.environ.get("BCFG_DOMAIN", "")
 BCFG_API_KEY = os.environ.get("BCFG_API_KEY", "")
 OPENAI_MODEL = os.environ.get("OPENAI_MODEL", "gpt-4o-mini")
+MODERATION_VALUES_FOR_BLOCKED = json.loads(
+    os.environ.get(
+        "MODERATION_VALUES_FOR_BLOCKED",
+        """{
+            "harassment": 0.5,
+            "harassment/threatening": 0.1,
+            "hate": 0.5,
+            "hate/threatening": 0.1,
+            "self-harm": 0.2,
+            "self-harm/instructions": 0.5,
+            "self-harm/intent": 0.7,
+            "sexual": 0.5,
+            "sexual/minors": 0.2,
+            "violence": 0.7,
+            "violence/graphic": 0.8,
+        }"""
+    )
+)
