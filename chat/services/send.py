296b3b05a389746c912225ca03aab039fc8830c9
abhay <sabhay@seas.upenn.edu>
2025-06-24 20:51:45 +0000

    Merged in merge/develop-into-staging (pull request #151)
    Merge/develop into staging
    
    * Merged in fix-summary-gpt-model-req (pull request #147)
    
    fix: missing 1 required positional argument: gpt_model
    
    * fix: missing 1 required positional argument: gpt_model
    
    * django update
    
    * fix: pytest factory boy version
    
    * wip
    
    * synk failure medium serrity request fix
    
    * fix test request netrc
    
    * fix test request netrc
    
    * ignore synk request untill aug
    
    * Merged in depreciate_mod_msg_send (pull request #149)
    
    fix: comment out send moderation message call
    
    * fix: comment out send moderation message call
    
    * pipenv update
    
    * chore: regenerate Pipfile.lock after merging develop

--- chat/services/send.py
+++ chat/services/send.py
@@ -46,12 +46,13 @@ def send_moderation_message(participant_id):
     Endpoint:
       POST /ai/api/participant/{id}/safety-plan/send
     """
-    url = f"{settings.BCFG_DOMAIN}/ai/api/participant/{participant_id}/safety-plan/send"
-    headers = {"Authorization": f"Bearer {settings.BCFG_API_KEY}"}
-    with httpx.Client() as client:
-        response = client.post(url, headers=headers)
-        response.raise_for_status()
-        return response.json()
+    # url = f"{settings.BCFG_DOMAIN}/ai/api/participant/{participant_id}/safety-plan/send"
+    # headers = {"Authorization": f"Bearer {settings.BCFG_API_KEY}"}
+    # with httpx.Client() as client:
+    #     response = client.post(url, headers=headers)
+    #     response.raise_for_status()
+    #     return response.json()
+    pass
 
 
 def send_school_summaries_to_hub_for_week(school_name: str, week_number: int, summary_contents: list[str]):
