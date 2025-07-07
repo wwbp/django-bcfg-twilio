8bfd05552cb2fd28f361b8b5566fd6da182f14c2
Abhay Singh <abhay97ps@gmail.com>
2025-07-07 12:35:16 -0400

    fix: lint

--- chat/admin.py
+++ chat/admin.py
@@ -251,7 +251,13 @@ class IndividualChatTranscriptAdmin(ReadonlyAdmin):
         "school_name",
     )
     search_fields = ("content",)
-    list_filter = ("role", "moderation_status", "created_at", ChatTranscriptWeekNumberFilter, IndividualChatTranscriptSchoolNameFilter)
+    list_filter = (
+        "role",
+        "moderation_status",
+        "created_at",
+        ChatTranscriptWeekNumberFilter,
+        IndividualChatTranscriptSchoolNameFilter,
+    )
     inlines = [IndividualPipelineRecordInline]
 
     def pipeline_record_link(self, obj):
@@ -322,7 +328,13 @@ class GroupChatTranscriptAdmin(ReadonlyAdmin):
     )
 
     search_fields = ("content",)
-    list_filter = ("role", "moderation_status", "created_at", ChatTranscriptWeekNumberFilter, GroupChatTranscriptSchoolNameFilter)
+    list_filter = (
+        "role",
+        "moderation_status",
+        "created_at",
+        ChatTranscriptWeekNumberFilter,
+        GroupChatTranscriptSchoolNameFilter,
+    )
     # inlines = [GroupPipelineRecordInline]
 
 
