a3962686539ff6f202cb168e06f5ea48cc25810a
Abhay Singh <abhay97ps@gmail.com>
2025-07-29 10:42:49 -0400

    block gpt call

--- chat/services/completion.py
+++ chat/services/completion.py
@@ -20,7 +20,8 @@ async def _generate_response_async(
     engine = OpenAIEngine(settings.OPENAI_API_KEY, model=gpt_model)
     try:
         assistant = Kani(engine, system_prompt=instructions, chat_history=chat_history)
-        response = await assistant.chat_round_str(message)
+        # response = await assistant.chat_round_str(message)
+        response = "mocked response"
         completion = await assistant.get_model_completion()
         return (
             response,
@@ -32,12 +33,10 @@ async def _generate_response_async(
 
 
 def _generate_response(chat_history, instructions, message, gpt_model):
-    loop = asyncio.new_event_loop()                   
+    loop = asyncio.new_event_loop()
     asyncio.set_event_loop(loop)
     try:
-        return loop.run_until_complete(
-            _generate_response_async(chat_history, instructions, message, gpt_model)
-        )
+        return loop.run_until_complete(_generate_response_async(chat_history, instructions, message, gpt_model))
     finally:
         loop.close()
         asyncio.set_event_loop(None)
