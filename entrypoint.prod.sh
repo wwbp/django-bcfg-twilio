4f7c3875b392518b7f1a69512ff7ada3aa1a212d
Abhay Singh <abhay97ps@gmail.com>
2025-07-29 22:23:00 -0400

    test larger worker more concurrency and gunicorn timeout

--- entrypoint.prod.sh
+++ entrypoint.prod.sh
@@ -7,7 +7,11 @@ gunicorn config.wsgi:application \
   --bind 0.0.0.0:8000 \
   --workers=5 \
   --worker-class=gthread \
-  --threads=4
+  --threads=4 \
+  --timeout=120 \
+  --keep-alive=5 \
+  --max-requests=1000 \
+  --max-requests-jitter=100
 
 
 
